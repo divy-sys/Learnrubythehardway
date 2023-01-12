c18a41e6720723486109839cc86bf55ae2138ccd
divy-sys <divy.varia.1020@gmail.com>
2023-01-12 17:42:57 +0530

    Today's work

---
+++ morepractise.rb
@@ -0,0 +1,12 @@
+def secret_things (started)
+    kit_beans = started * 100
+    jars = kit_beans * 1000
+    crates = jars/100
+    return kit_beans, jars, crates
+end
+
+point = 10000
+beans, jars, crates = secret_things(point)
+
+puts "With a starting point of #{point}"
+puts "we have #{beans}, #{jars}jars and #{crates}crates"
